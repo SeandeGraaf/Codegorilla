1398f9baf5daa103e38b4a6b3de618e8e0fab353
SeandeGraaf <126236701+SeandeGraaf@users.noreply.github.com>
2023-02-24 09:31:53 +0100

    Add files via upload
    My first upload on GitHub

---
+++ src/main.js
@@ -0,0 +1,100 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyA") {
+        let Akey = new Audio("sounds/white_keys/A.mp3");
+        Akey.currentTime = 0;
+        Akey.play();
+        }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyS") {
+        let Skey = new Audio("sounds/white_keys/S.mp3");
+        Skey.currentTime = 0;
+        Skey.play();
+        }
+
+
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyD") {
+        let Dkey = new Audio("sounds/white_keys/D.mp3");
+        Dkey.currentTime = 0;
+        Dkey.play();
+        }
+});
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyF") {
+        let Fkey = new Audio("sounds/white_keys/F.mp3");
+        Fkey.currentTime = 0;
+        Fkey.play()
+        }
+});
+    ;
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyG") {
+        let Gkey = new Audio("sounds/white_keys/G.mp3");
+        Gkey.currentTime = 0;
+        Gkey.play();
+        }
+});
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyH") {
+        let Hkey = new Audio("sounds/white_keys/H.mp3");
+        Hkey.currentTime = 0;
+        Hkey.play();
+        }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyJ") {
+        let Jkey = new Audio("sounds/white_keys/J.mp3");
+        Jkey.currentTime = 0;
+        Jkey.play();
+        }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyW") {
+        let Wkey = new Audio("sounds/black_keys/W.mp3");
+        Wkey.currentTime = 0;
+        Wkey.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyE") {
+        let Ekey = new Audio("sounds/black_keys/E.mp3");
+        Ekey.currentTime = 0;
+        Ekey.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyT") {
+        let Tkey = new Audio("sounds/black_keys/T.mp3");
+        Tkey.currentTime = 0;
+        Tkey.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyY") {
+        let Ykey = new Audio("sounds/black_keys/Y.mp3");
+        Ykey.currentTime = 0;
+        Ykey.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyU") {
+        let Ukey = new Audio("sounds/black_keys/U.mp3");
+        Ukey.currentTime = 0;
+        Ukey.play();
+    }
+});
